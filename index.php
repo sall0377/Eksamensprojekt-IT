8226cb2ceba528e33ea254a4bfa2a3e91bc5e5c0
sall0377 <sallywinther@gmail.com>
2018-02-22 11:58:27 +0100

    Import af filer

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Eksamensprojekt</title>
+    <link rel="stylesheet" href="CSS/style.css">
+  </head>
+
+  <body>
+      <div id="wrapper"> 
+        <div id="header">
+            <img src="Billeder/testimonial-background-landscape-450Oc.jpg" id="logo">
+            <div id="buttons">
+                <input type="button" value="Log in" onclick="window.location.href='index.php'" class="knap">
+                <input type="button" value="Ønskeliste" onclick="window.location.href='index.php'" class="knap">
+                <input type="button" value="Kurv" onclick="window.location.href='index.php'" class="knap">
+            </div>
+          </div>
+          <div id="menu">
+          
+          </div>
+      </div>
+    
+      
+  </body>
+
+</html>
